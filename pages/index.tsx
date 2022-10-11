8628bc2c6cdbb2bbdfd294aa16c439913b2f663a
Josh Daniel <joshxfi.dev@gmail.com>
2022-10-11 11:55:15 +0800

    feat: add card animation

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,25 +1,36 @@
 /* eslint-disable @next/next/no-img-element */
 import type { NextPage } from "next";
+import Hover from "react-3d-hover";
 
 const Home: NextPage = () => {
   return (
-    <section className="flex justify-center">
-      <div className="relative">
-        <div className="absolute left-0 right-0 top-32 mx-auto text-center text-[#676c72]">
-          <p className="text-4xl font-bold">Josh Daniel</p>
-          <p className="text-2xl">Bañares</p>
-          <p className="mt-12 text-xl">
-            Technology <br />
-            Co-Lead
-          </p>
+    <section className="flex flex-col items-center">
+      <div className="w-[350px] mx-auto w-full font-semibold text-5xl mb-8">
+        <h1>SHEESH,</h1>
+        <div className='relative'>
+        <h1 className='z-10 relative'>Here&apos;s your ID!</h1>
+          <div className='w-[190px] h-[12px] bg-primary absolute right-1 bottom-1' />
         </div>
-
-        <img
-          className="w-full max-w-[432px] object-contain rounded-md"
-          src="/assets/id_1.png"
-          alt="id"
-        />
       </div>
+
+      <Hover perspective={900}>
+        <div className="relative">
+          <div className="absolute left-0 right-0 top-32 mx-auto text-center text-[#676c72]">
+            <p className="text-3xl font-bold">Josh Daniel</p>
+            <p className="text-xl">Bañares</p>
+            <p className="mt-12 text-lg">
+              Technology <br />
+              Co-Lead
+            </p>
+          </div>
+
+          <img
+            className="w-full max-w-[350px] object-contain rounded-md"
+            src="/assets/id_1.png"
+            alt="id"
+          />
+        </div>
+      </Hover>
     </section>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 export const Navbar = () => {
   return (
-    <nav className="py-6 flex items-center justify-between text-sm">
+    <nav className="pt-6 mb-16 flex items-center justify-between text-sm">
       <div className="flex items-center space-x-2">
         <Image
           src="/assets/gdsc_logo.png"
