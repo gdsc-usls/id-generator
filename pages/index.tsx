9d2aa58f38e946b3639a32d58084c76a23525d0d
Josh Daniel <joshxfi.dev@gmail.com>
2022-10-11 16:09:08 +0800

    feat: add student id section

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,34 +1,41 @@
-import { Card } from "@/components";
 import type { NextPage } from "next";
-import Hover from "react-3d-hover";
+import { useRouter } from "next/router";
 
 const Home: NextPage = () => {
+  const { push } = useRouter();
   return (
-    <section className="flex flex-col items-center w-[350px] mx-auto">
+    <section>
       <div className="font-semibold text-5xl mb-8 self-start">
-        <h1>SHEESH,</h1>
+        <h1>Enter your USLS</h1>
         <div className="relative">
-          <h1 className="z-10 relative">Here&apos;s your ID!</h1>
-          <div className="w-[190px] h-[12px] bg-primary absolute -right-2 bottom-1" />
+          <h1 className="z-10 relative">Student ID</h1>
+          <div className="w-[250px] h-[12px] bg-primary absolute -left-1 bottom-1" />
         </div>
       </div>
 
-      <div className="hidden lg:block">
-        <Hover perspective={900}>
-          <Card />
-        </Hover>
-      </div>
-
-      <div className="lg:hidden">
-        <Card />
-      </div>
-
-      <button
-        type="button"
-        className="self-end mt-4 hover:underline outline-none"
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          push("/result");
+        }}
+        className="flex space-x-3"
       >
-        Download Image
-      </button>
+        <input
+          type="text"
+          required
+          minLength={7}
+          maxLength={7}
+          className="outline-none rounded-md border border-secondary-100 px-4 py-3 bg-transparent"
+          placeholder="Student ID"
+        />
+
+        <button
+          type="submit"
+          className="bg-primary rounded-md px-6 flex-none py-3 outline-none"
+        >
+          Generate ID
+        </button>
+      </form>
     </section>
   );
 };
---
+++ pages/result.tsx
@@ -0,0 +1,36 @@
+import type { NextPage } from "next";
+import Hover from "react-3d-hover";
+import { Card } from "@/components";
+
+const Result: NextPage = () => {
+  return (
+    <section className="flex flex-col items-center w-[350px] mx-auto">
+      <div className="font-semibold text-5xl mb-8 self-start">
+        <h1>SHEESH,</h1>
+        <div className="relative">
+          <h1 className="z-10 relative">Here&apos;s your ID!</h1>
+          <div className="w-[190px] h-[12px] bg-primary absolute -right-2 bottom-1" />
+        </div>
+      </div>
+
+      <div className="hidden lg:block">
+        <Hover perspective={900}>
+          <Card />
+        </Hover>
+      </div>
+
+      <div className="lg:hidden">
+        <Card />
+      </div>
+
+      <button
+        type="button"
+        className="self-end mt-4 hover:underline outline-none"
+      >
+        Download Image
+      </button>
+    </section>
+  );
+};
+
+export default Result;
