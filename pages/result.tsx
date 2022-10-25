2fc7267b001e6800d7635125c21cf13551555dc9
Josh Daniel <joshxfi.dev@gmail.com>
2022-10-25 16:28:52 +0800

    feat: add download image

--- pages/result.tsx
+++ pages/result.tsx
@@ -1,10 +1,30 @@
-import React from "react";
-import type { NextPage } from "next";
+import React, { useRef, useCallback } from "react";
 import Hover from "react-3d-hover";
+import type { NextPage } from "next";
+import { toPng } from "html-to-image";
 
 import { Card } from "@/components";
 
 const Result: NextPage = () => {
+  const cardRef = useRef<HTMLDivElement>(null);
+
+  const handleDownload = useCallback(() => {
+    if (cardRef.current === null) {
+      return;
+    }
+
+    toPng(cardRef.current, { cacheBust: true })
+      .then((dataUrl) => {
+        const link = document.createElement("a");
+        link.download = "gdsc_id.png";
+        link.href = dataUrl;
+        link.click();
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  }, [cardRef]);
+
   return (
     <section className="flex flex-col items-center w-[350px] mx-auto">
       <div className="font-semibold text-5xl mb-8 self-start">
@@ -15,18 +35,21 @@ const Result: NextPage = () => {
         </div>
       </div>
 
-      <div className="hidden lg:block">
-        <Hover perspective={900}>
-          <Card />
-        </Hover>
-      </div>
+      <div ref={cardRef}>
+        <div className="hidden lg:block">
+          <Hover perspective={900}>
+            <Card />
+          </Hover>
+        </div>
 
-      <div className="lg:hidden">
-        <Card />
+        <div className="lg:hidden">
+          <Card />
+        </div>
       </div>
 
       <button
         type="button"
+        onClick={handleDownload}
         className="self-end mt-4 hover:underline outline-none"
       >
         Download Image
