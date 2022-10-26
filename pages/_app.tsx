eba23d0820e80fc3be6de77195f02d83fa48a15e
Josh Daniel <joshxfi.dev@gmail.com>
2022-10-26 14:11:39 +0800

    chore: add react-hot-toast

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { Toaster } from "react-hot-toast";
 import { Layout } from "@/components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <Toaster />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }
 
