3763601a8c76d3110400d5794c74a583a9f4cabe
Josh Daniel <joshxfi.dev@gmail.com>
2022-10-29 20:42:09 +0800

    chore: revert to img

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -1,12 +1,12 @@
+/* eslint-disable @next/next/no-img-element */
 import React from "react";
-import Image from "next/image";
 
 export const Card = ({ data }: { data: Member }) => {
   const position = data.position.split(" ");
 
   return (
     <div className="relative">
-      <div className="absolute left-0 right-0 top-32 mx-auto text-center text-[#676c72] z-10">
+      <div className="absolute left-0 right-0 top-32 mx-auto text-center text-[#676c72]">
         <p className="text-3xl font-bold">{data.firstName}</p>
         <p className="text-xl">{data.lastName}</p>
 
@@ -24,13 +24,10 @@ export const Card = ({ data }: { data: Member }) => {
         </div>
       </div>
 
-      <Image
-        height={500}
-        width={350}
-        objectFit="contain"
-        className="rounded-md pointer-events-none"
+      <img
+        className="w-full object-contain rounded-md pointer-events-none"
         src={position[0] == "Chief" ? "/assets/id_2.png" : "/assets/id_1.png"}
-        alt="gdsc id"
+        alt="id"
       />
     </div>
   );
