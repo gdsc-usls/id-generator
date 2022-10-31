da14a4503a016e7aa84b180325099a2c62796d2c
Josh Daniel <joshxfi.dev@gmail.com>
2022-10-31 21:14:29 +0800

    chore: update footer

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -2,9 +2,16 @@ import React from "react";
 
 export const Footer = () => {
   return (
-    <footer className='text-center pb-12 pt-24'>
+    <footer className="text-center pb-12">
       <p>Google Developer Student Clubs - USLS © 2022</p>
-      <p className='text-secondary-100 text-sm'>Developed by Josh Daniel Bañares</p>
+      <a
+        href="https://github.com/joshxfi"
+        target="_blank"
+        rel="noopener noreferrer"
+        className="text-secondary-100 text-sm hover:underline"
+      >
+        Developed by Josh Daniel Bañares
+      </a>
     </footer>
   );
 };
