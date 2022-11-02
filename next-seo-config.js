cc388b3a0818a3f58d1f7ad3ef374ded0b908d75
Josh Daniel <joshxfi.dev@gmail.com>
2022-11-02 10:25:08 +0800

    feat: add seo

---
+++ next-seo-config.js
@@ -0,0 +1,28 @@
+const description =
+  "ID Badge Generator for Google Developer Student Clubs - USLS";
+
+const SEO = {
+  title: "GDSC USLS ID Badge",
+  description,
+  openGraph: {
+    type: "website",
+    url: "https://gdsc-usls-id.vercel.app",
+    title: "Generate your ID Badge, Googusls!",
+    description,
+    images: [
+      {
+        url: "/assets/banner.png",
+        width: 1194,
+        height: 795,
+        alt: "GDSC banner",
+        type: "image/png",
+      },
+    ],
+    site_name: "GDSC USLS ID Badge Generator",
+  },
+  twitter: {
+    cardType: "summary_large_image",
+  },
+};
+
+export default SEO;
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,15 @@
 import "../styles/globals.css";
+import { DefaultSeo } from "next-seo";
 import type { AppProps } from "next/app";
 import { Toaster } from "react-hot-toast";
+
+import SEO from "../next-seo-config";
 import { Layout } from "@/components";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <>
+      <DefaultSeo {...SEO} />
       <Toaster />
       <Layout>
         <Component {...pageProps} />
