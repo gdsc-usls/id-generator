1b503286b7a02cd463e4680a84763e34ac3eb01a
Josh Daniel <joshxfi.dev@gmail.com>
2022-11-08 09:46:04 +0800

    refactor: enlarge id on lg breakpoint

--- pages/member/[id].tsx
+++ pages/member/[id].tsx
@@ -53,12 +53,7 @@ const Member = () => {
   }
 
   return (
-    <section className="flex flex-col items-center w-[350px] mx-auto">
-      <div className="relative font-semibold text-5xl mb-8 self-start">
-        <h1 className="z-10 relative">Here&apos;s your ID!</h1>
-        <div className="w-[190px] h-[12px] bg-primary-100 absolute -right-2 bottom-1" />
-      </div>
-
+    <section className="flex flex-col items-center w-[350px] md:w-[400px] mx-auto md:mt-24">
       <div ref={cardRef}>
         <Hover perspective={900}>
           <Card data={data} />
